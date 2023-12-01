e73504ba37ff3847e58a151e2415a151f74726e5
Gueguette_huawei <gaetan.jenni@gmail.com>
2023-12-01 16:30:53 +0100

    all compile great

--- ModelSim/projectSemester/src/SIPO_shift_reg.vhdl
+++ ModelSim/projectSemester/src/SIPO_shift_reg.vhdl
@@ -29,28 +29,35 @@ architecture mix of SIPO_shift_reg is
 
 	signal SIPO_data_s : std_logic_vector(DATA_BUS_WIDTH_c downto 0);
 
-	begin 
-		process (rst_i, clk_i)
-			if rst_i = '1' then
-				SIPO_data_s <= (others => '0');
+	signal clk_and_en_s : std_logic;
+	signal shift_and_en_s : std_logic;
 
-			elsif rising_edge(clk_i) then
-				SIPO_data_s(0) <= data_i;
-				SIPO: for i in DATA_BUS_WIDTH_c downto 1 generate
-					begin
-						SIPO_reg: DFF port map(
-							D_i => SIPO_data_s(i-1),
-							rst_i => rst_i,
-							clk_i => (clk_i and en_i),
-							Q_o => SIPO_data_s(i)
-						);
-						memory_reg: DFF port map(
-							D_i => SIPO_data_s(i),
-							rst_i => rst_i,
-							clk_i => (shift_i and en_i),
-							Q_o => data_o(i-1)
-						);
-				end generate;
-			end if;
+	begin
+		clk_and_en_s <= (clk_i and en_i);
+		shift_and_en_s <= (shift_i and en_i);
+		process (rst_i, clk_i)
+			begin
+				if rst_i = '1' then
+					SIPO_data_s <= (others => '0');
+				elsif rising_edge(clk_i) then
+					SIPO_data_s(0) <= data_i;
+				end if;
 		end process;
+		
+		SIPO: for i in DATA_BUS_WIDTH_c downto 1 generate
+			begin
+				SIPO_reg: DFF port map(
+					D_i => SIPO_data_s(i-1),
+					rst_i => rst_i,
+					clk_i => clk_and_en_s,
+					Q_o => SIPO_data_s(i)
+				);
+				memory_reg: DFF port map(
+					D_i => SIPO_data_s(i),
+					rst_i => rst_i,
+					clk_i => shift_and_en_s,
+					Q_o => data_o(i-1)
+				);
+		end generate;
+
 end architecture;
--- ModelSim/projectSemester/src/data_flow.vhdl
+++ ModelSim/projectSemester/src/data_flow.vhdl
@@ -5,35 +5,39 @@ use work.my_pkg.all;
 
 entity data_flow is
     port(
-        en_i: std_logic,
-        rst_i: std_logic,
-        clk_i: std_logic,
-        shift_o: std_logic,
-        enSIPO_o: std_logic,
-        enPISO_o: std_logic
+        en_i: in std_logic;
+        rst_i: in std_logic;
+        clk_i: in std_logic;
+        shift_o: out std_logic;
+        enSIPO_o: out std_logic;
+        enPISO_o: out std_logic
     );
 end entity;
 
 architecture mix of data_flow is
 
-    variable nClk_v : integer := 0;
+    signal shift_s : std_logic;
 
+    signal nClk_s : integer := 0;
+
+begin
+    enSIPO_o <= en_i;
+    enPISO_o <= en_i;
+
+    shift_o <= shift_s;
+
+    process(rst_i, clk_i)
     begin
-        enSIPO_o <= en_i;
-        enPISO_o <= en_i;
-
-        process(rst_i, clk_i)
-            begin
-                if (rst_i = '1') then
-                    nClk_v <= 0;
-                    
-                elsif(nClk_v >= DATA_BUS_WIDTH_c) then
-                    shift_o <= 1;
-                    nClk_v <= 0;
-                else
-                    shift_o <= 0;
-                    nClk_v <= nClk_v + 1;
-                end if;
-        end process;
+        if (rst_i = '1') then
+            shift_s <= '0';
+            nClk_s <= 0;
+        elsif(nClk_s >= DATA_BUS_WIDTH_c) then
+            shift_s <= '1';
+            nClk_s <= 0;
+        else
+            shift_s <= '0';
+            nClk_s <= nClk_s + 1;
+        end if;
+    end process;
 
 end architecture;
